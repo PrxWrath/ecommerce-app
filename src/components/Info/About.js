ba7fb832fab3fef695c4458ef8f01efbdc1bada5
Pratyush Tiwari <pratyush.tiwari@aajtak.com>
2023-05-13 15:10:28 +0530

    Made minor changes for testing

--- src/components/Info/About.js
+++ src/components/Info/About.js
@@ -5,7 +5,7 @@ const About = () => {
   return (
     <Container>
         <Card className="bg-dark text-light w-50 mx-auto text-center my-2 shadow-lg p-3">
-            <Card.Title>TechStop - A one stop solution to all your techie needs</Card.Title>
+            <Card.Title>TechStop - A one stop solution for all your techie needs</Card.Title>
             <Card.Body>
                 <Card.Text>
                     Established in 2022, techstop is an online mart for all your nerdy cravings. Techstop was founded Pratyush Tiwari, an IT enthusisast like you to facilitate like minded individuals with an easy and quick shooping solution
